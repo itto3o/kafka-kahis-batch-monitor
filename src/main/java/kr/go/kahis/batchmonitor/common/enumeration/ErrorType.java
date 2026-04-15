efb30d6585a6aa809f4f6ca4e4b9072b1d3cf789
itto3o <208ehd@naver.com>
2026-04-15 14:21:08 +0900

    분석이 필요/불필요로 에러유형 구분, 불필요한 에러 유형의 토픽을 모두 반환하는 함수 구현

--- src/main/java/kr/go/kahis/batchmonitor/common/enumeration/ErrorType.java
+++ src/main/java/kr/go/kahis/batchmonitor/common/enumeration/ErrorType.java
@@ -1,23 +1,34 @@
 package kr.go.kahis.batchmonitor.common.enumeration;
 
+import java.util.Arrays;
 import lombok.Getter;
 
 public enum ErrorType {
 
-  LIVESTOCK_ANOMALY("error.livestock-anomaly"),
-  PREDICTION_ANOMALY("error.prediction-anomaly"),
-  FARM_COUNT_ANOMALY("error.data-sync-failure"),
-  PNU_ANOMALY("error.data-sync-failure"),
-  FARM_COORDINATE_MISSING("error.data-sync-failure"),
-  DATA_NOT_FOUND("error.data-sync-failure"),
-  TRAININGSET_COUNT_MISMATCH("error.data-sync-failure"),
-  CALC_ENV_ANOMALY("error.data-sync-failure"),
-  UNKNOWN("error.data-sync-failure");
+  LIVESTOCK_ANOMALY("error.livestock-anomaly", true),
+  PREDICTION_ANOMALY("error.prediction-anomaly", false),
+  FARM_COUNT_ANOMALY("error.farm-count-anomaly", false),
+  PNU_ANOMALY("error.pnu-anomaly", false),
+  FARM_COORDINATE_MISSING("error.farm-coordinate-missing", false),
+  DATA_NOT_FOUND("error.data-not-found", false),
+  TRAININGSET_COUNT_MISMATCH("error.trainingset-count-mismatch", false),
+  CALC_ENV_ANOMALY("error.calc-env-anomaly", false),
+  UNKNOWN("error.unknown", false);
 
   @Getter
   private final String topic;
+  @Getter
+  private final boolean isNeedAnalysis;
 
-  ErrorType(String topic) {
+  ErrorType(String topic, Boolean isNeedAnalysis) {
     this.topic = topic;
+    this.isNeedAnalysis = isNeedAnalysis;
+  }
+
+  public String[] notAnalysisTopics() {
+    return Arrays.stream(ErrorType.values())
+        .filter(type -> !type.isNeedAnalysis())
+        .map(ErrorType::getTopic)
+        .toArray(String[]::new);
   }
 }
